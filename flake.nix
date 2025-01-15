907589606630cfe4349f0cd58263878afbb790cc
Cameron <cbadwork@gmail.com>
2025-01-15 06:17:07 +0000

    fixed flake to have home manager

--- flake.nix
+++ flake.nix
@@ -5,15 +5,20 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, sops-nix }: {
+  outputs = { self, nixpkgs, sops-nix, home-manager }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         ./hardware-configuration.nix
         sops-nix.nixosModules.sops
+        home-manager.nixosModules.home-manager
         {
           imports = [ ./modules ];
         }
@@ -21,3 +26,4 @@
     };
   };
 }
+
