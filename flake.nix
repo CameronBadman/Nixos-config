5ac050d683eb111951cfdc195a8e96b4186686c5
Cameron <cbadwork@gmail.com>
2025-01-16 12:24:07 +0000

    added secrets for testing agenix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
-  description = "Your NixOS configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Add agenix
+    agenix.url = "github:ryantm/agenix";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, agenix, ... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
+          agenix.nixosModules.default  # Add this line
         ];
         specialArgs = { inherit inputs self; };
       };
