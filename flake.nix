4756ebb374221406b1fdf40391100c0751ca9032
Cameron <cbadwork@gmail.com>
2025-01-16 03:38:31 +0000

    reverting flake

--- flake.nix
+++ flake.nix
@@ -3,23 +3,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, agenix, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          ./modules/secrets.nix
-          agenix.nixosModules.default
           home-manager.nixosModules.home-manager
         ];
         specialArgs = { inherit inputs self; };
