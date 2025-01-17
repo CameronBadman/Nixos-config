71452fb7e53f31d051b3e8a207aceb9db0c7ed16
Cameron <cbadwork@gmail.com>
2025-01-17 01:18:14 +0000

    feat: add nvf (neovim framework) to flake inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "nixos config";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
@@ -11,9 +10,13 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Add nvf input
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  outputs = { self, nixpkgs, home-manager, sops-nix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, sops-nix, nvf, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -27,9 +30,11 @@
             ./configuration.nix
             ./hosts
             ./modules
-            home-manager.nixosModules.home-manager  
-            ./users                     
+            home-manager.nixosModules.home-manager
+            ./users
             sops-nix.nixosModules.sops
+            # Add nvf module
+            nvf.nixosModules.default
           ];
         };
       };
--- modules/programs/development/nvim/neovim.nix
+++ modules/programs/development/nvim/neovim.nix
@@ -1,124 +1,70 @@
-{ config, lib, pkgs, ... }:
-{
-  environment.etc = {
-    "xdg/nvim".source = pkgs.fetchFromGitHub {
-      owner = "CameronBadman";
-      repo = "nvim-config";
-      rev = "main";
-      sha256 = "0wld074v5r13gij32jgrpdjd8r767rrcpbqwj8i8ffiffx88ykx6";
-    };
-  };
+{ config, lib, pkgs, inputs, ... }:
 
-  # Add required system packages
-  environment.systemPackages = with pkgs; [
-    # Language Servers
-    gopls
-    rust-analyzer
-    nodePackages.typescript-language-server
-    lua-language-server
-    nil # Nix LSP
-    
-    # Required dependencies
-    python311Packages.pip
-    luarocks
-    
-    # Development tools
-    ripgrep
-    fd
-    
-    # For clipboard support
-    wl-clipboard
+{
+  imports = [
+    inputs.nvf.nixosModules.default
   ];
 
-  programs.neovim = {
+  programs.nvf = {
     enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    configure = {
-      customRC = ''
-        lua << EOF
-        -- Set up XDG paths
-        vim.env.XDG_CONFIG_HOME = '/var/lib/nvim/config'
-        vim.env.XDG_DATA_HOME = '/var/lib/nvim/data'
-        vim.env.XDG_STATE_HOME = '/var/lib/nvim/state'
-        vim.env.XDG_CACHE_HOME = '/var/lib/nvim/cache'
-        
-        -- Create config directory if it doesn't exist
-        local config_dir = '/var/lib/nvim/config/nvim'
-        if vim.fn.isdirectory(config_dir) == 0 then
-          vim.fn.mkdir(config_dir, 'p')
-        end
-        
-        -- Set up lazy.nvim paths
-        vim.g.lazy_path = '/var/lib/nvim/lazy'
-        vim.g.lazy_config = {
-          root = vim.g.lazy_path,
-          lockfile = vim.g.lazy_path .. '/lazy-lock.json',
-          state = vim.g.lazy_path .. '/state.json',
-          readme = {
-            root = vim.g.lazy_path .. '/readme',
-          },
-        }
+    
+    # Your existing system packages
+    extraPackages = with pkgs; [
+      gopls
+      rust-analyzer
+      nodePackages.typescript-language-server
+      lua-language-server
+      nil
+      python311Packages.pip
+      luarocks
+      ripgrep
+      fd
+      wl-clipboard
+    ];
 
-        -- Disable luarocks warning
-        vim.g.lazy_rocks = {
-          enabled = false
-        }
+    settings = {
+      vim = {
+        viAlias = true;
+        vimAlias = true;
+        defaultEditor = true;
+      };
 
-        -- Set up Mason and LSP paths
-        vim.env.MASON_INSTALL_DIR = '/var/lib/nvim/mason'
-        vim.env.LSP_SERVERS_DIR = '/var/lib/nvim/lsp'
-        
-        -- Add config directories to runtimepath
-        vim.opt.runtimepath:prepend("/etc/xdg/nvim")
-        vim.opt.runtimepath:prepend(vim.g.lazy_path)
-        vim.opt.runtimepath:append("/etc/xdg/nvim/lua")
-        
-        -- Add lua path for plugins
-        package.path = package.path .. ";/etc/xdg/nvim/lua/?.lua"
-        package.path = package.path .. ";/etc/xdg/nvim/lua/?/init.lua"
-        
-        -- Set up clipboard
-        vim.g.clipboard = {
-          name = 'wl-clipboard',
-          copy = {
-            ['+'] = 'wl-copy',
-            ['*'] = 'wl-copy',
-          },
-          paste = {
-            ['+'] = 'wl-paste',
-            ['*'] = 'wl-paste',
-          },
-          cache_enabled = 1,
-        }
-        
-        -- Load init.lua
-        dofile("/etc/xdg/nvim/init.lua")
-        EOF
-      '';
-      packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [ 
-          # ... your existing plugins ...
-        ];
+      # Core settings
+      clipboard.enable = true;
+      clipboard.provider = "wl-clipboard";
+    };
+
+    # LSP configurations
+    lsp = {
+      enable = true;
+      servers = {
+        gopls.enable = true;
+        rust-analyzer.enable = true;
+        tsserver.enable = true;
+        lua-ls.enable = true;
+        nil.enable = true;
       };
     };
   };
 
-  # Directory structure with additional directories
-  systemd.tmpfiles.rules = [
-    # Base directories
-    "d /var/lib/nvim 0755 root root -"
-    "d /var/lib/nvim/config 0750 cameron cameron -"
-    "d /var/lib/nvim/config/nvim 0750 cameron cameron -" # Add this line
-    "d /var/lib/nvim/data 0750 cameron cameron -"
-    "d /var/lib/nvim/state 0750 cameron cameron -"
-    "d /var/lib/nvim/cache 0750 cameron cameron -"
-    "d /var/lib/nvim/lazy 0750 cameron cameron -"
-    "d /var/lib/nvim/lsp 0750 cameron cameron -"
-    "d /var/lib/nvim/mason 0750 cameron cameron -"
-    "d /var/lib/nvim/undo 0750 cameron cameron -"
-    "d /var/lib/nvim/swap 0750 cameron cameron -"
-    "d /var/lib/nvim/backup 0750 cameron cameron -"
-  ];
+  # Your existing systemd tmpfiles rules can be converted to:
+  environment.persistence = lib.mkIf config.programs.nvf.enable {
+    "/var/lib/nvim" = {
+      user = "cameron";
+      group = "cameron";
+      mode = "0750";
+      directories = [
+        "config"
+        "data"
+        "state"
+        "cache"
+        "lsp"
+        "mason"
+        "undo"
+        "swap"
+        "backup"
+      ];
+    };
+  };
 }
+
