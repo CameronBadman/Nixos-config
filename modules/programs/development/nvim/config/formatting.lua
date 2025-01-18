c9023e0078ccf87cb31ae5595340880f984c1761
Cameron <cbadwork@gmail.com>
2025-01-18 12:20:15 +0000

    adding formatting config

--- modules/programs/development/nvim/config/formatting.lua
+++ modules/programs/development/nvim/config/formatting.lua
@@ -1,41 +1,43 @@
-require("conform").setup({
-  -- Format options
-  formatters_by_ft = {
-    -- Web Development
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    css = { "prettier" },
-    html = { "prettier" },
-    json = { "prettier" },
-    yaml = { "prettier" },
-    markdown = { "prettier" },
+local M = {}
 
-    -- Systems Programming
-    rust = { "rustfmt" },
-    c = { "clang-format" },
-    cpp = { "clang-format" },
-    go = { "gofmt", "goimports" },
+function M.setup()
+	require("conform").setup({
+		-- Format options
+		formatters_by_ft = {
+			-- Web Development
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			-- Systems Programming
+			rust = { "rustfmt" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+			go = { "gofmt", "goimports" },
+			-- Scripting Languages
+			python = { "black" },
+			lua = { "stylua" },
+			sh = { "shfmt" },
+			-- Configuration
+			nix = { "nixfmt" },
+		},
+		-- Format on save
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	})
 
-    -- Scripting Languages
-    python = { "black" },
-    lua = { "stylua" },
-    sh = { "shfmt" },
+	-- Format keymap
+	vim.keymap.set({ "n", "v" }, "<leader>f", function()
+		require("conform").format({
+			async = true,
+			lsp_fallback = true,
+		})
+	end, { desc = "Format file or range (in visual mode)" })
+end
 
-    -- Configuration
-    nix = { "nixfmt" },
-  },
-
-  -- Format on save
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-})
-
--- Format keymap
-vim.keymap.set({ "n", "v" }, "<leader>f", function()
-  require("conform").format({
-    async = true,
-    lsp_fallback = true,
-  })
-end, { desc = "Format file or range (in visual mode)" })
+return M
