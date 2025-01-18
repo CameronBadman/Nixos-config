462568a42b19b15d9bb948a40d966f617580952c
Cameron <cbadwork@gmail.com>
2025-01-18 02:07:11 +0000

    fixed xdg file adding to be recusive

--- modules/programs/development/nvim/neovim.nix
+++ modules/programs/development/nvim/neovim.nix
@@ -9,19 +9,42 @@
       vimAlias = true;
 
       extraPackages = with pkgs; [
-        gopls
-        rust-analyzer
-        nodePackages.typescript-language-server
-        lua-language-server
-        nil
-        python311Packages.pip
-        luarocks
-        ripgrep
-        fd
-        wl-clipboard
-	kubectl
-      ];
+        gopls # Go language server
+        rust-analyzer # Rust language server
+        nodePackages.typescript-language-server # TypeScript/JavaScript language server
+        lua-language-server # Lua language server
+        nil # Nix language server
+        sonarlint-ls # SonarLint language server
 
+        # Language Tools & Package Managers
+        python311Packages.pip # Python package manager
+        luarocks # Lua package manager
+
+        # Code Formatters
+        black # Python formatter
+        stylua # Lua formatter
+        nodePackages.prettier # JavaScript/TypeScript/JSON/YAML/HTML/CSS formatter
+        shfmt # Shell script formatter
+        nixfmt # Nix formatter
+
+        # Linters & Code Quality
+        codespell # Spell checker for code
+
+        # System Tools
+        ripgrep # Fast grep replacement
+        fd # Fast find replacement
+        wl-clipboard # Wayland clipboard utility
+
+        # Kubernetes Tools
+        kubectl # Kubernetes command-line tool
+
+        # Additional C/C++ Tools (from previous discussion)
+        clang-tools # Includes clang-format
+        cpplint # Google's C++ linter
+
+        # Additional Go Tools (from previous discussion)
+        golangci-lint # Go linter suite
+      ];
       plugins = with pkgs.vimPlugins; [
         # Package management
         lazy-nvim
@@ -29,7 +52,9 @@
         # Core plugins
         plenary-nvim
         nvim-web-devicons
-        
+        conform-nvim
+        nvim-lint
+
         # LSP Support
         nvim-lspconfig
         mason-nvim
@@ -50,173 +75,137 @@
 
         # File navigation and UI
         telescope-nvim
-	telescope-fzf-native-nvim
+        telescope-fzf-native-nvim
         neo-tree-nvim
         lualine-nvim
 
-	{
-	plugin = pkgs.vimUtils.buildVimPlugin {
-	name = "kubectl-nvim";
-	src = inputs.kubectl-nvim;
-	    };
-	type = "lua";  # Add this line
-	  }
-
-	catppuccin-nvim
-        
-        # Syntax and languages
-        (nvim-treesitter.withPlugins (plugins: with plugins; [
-	    lua
-	    python
-	    typescript
-	    javascript
-	    go
-	    rust
-	    c
-	    cpp
-	    java
-	    json
-	    yaml
-	    toml
-	    html
-	    css
-	    markdown
-	    markdown_inline
-	    vim
-	    vimdoc
-	    query
-	  ]))
-	];
-        
-      extraLuaConfig = ''
-        -- Basic vim options
-        vim.g.mapleader = " "
-        vim.g.maplocalleader = " "
-
-        -- Options
-        vim.opt.number = true
-        vim.opt.relativenumber = true
-        vim.opt.mouse = 'a'
-        vim.opt.showmode = false
-        vim.opt.clipboard = 'unnamedplus'
-        vim.opt.breakindent = true
-        vim.opt.undofile = true
-        vim.opt.ignorecase = true
-        vim.opt.smartcase = true
-        vim.opt.signcolumn = 'yes'
-        vim.opt.updatetime = 250
-        vim.opt.timeoutlen = 300
-        vim.opt.splitright = true
-        vim.opt.splitbelow = true
-        vim.opt.list = true
-        vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-        vim.opt.inccommand = 'split'
-        vim.opt.cursorline = true
-        vim.opt.scrolloff = 10
-        vim.opt.hlsearch = false
-
-        -- Basic Keymaps
-        vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-        -- Theme setup
-        vim.cmd.colorscheme("catppuccin")
-
-        -- LSP Configuration
-        local lspconfig = require('lspconfig')
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-        -- LSP servers setup
-        local servers = {
-          gopls = {},
-          rust_analyzer = {},
-          ts_ls = {},  -- Changed from tsserver
-          lua_ls = {
-            settings = {
-              Lua = {
-                diagnostics = { globals = {'vim'} }
-              }
-            }
-          },
-          nil_ls = {},
+        {
+          plugin = pkgs.vimUtils.buildVimPlugin {
+            name = "kubectl-nvim";
+            src = inputs.kubectl-nvim;
+          };
+          type = "lua"; # Add this line
         }
 
-        -- Simple LSP setup for now, we'll move this to its own file later
-        for server, config in pairs(servers) do
-          if server == "ts_ls" then  -- Special case for typescript
-            config.capabilities = capabilities
-            config.root_dir = lspconfig.util.root_pattern("package.json")
-            lspconfig[server].setup(config)
-          else
-            config.capabilities = capabilities
-            lspconfig[server].setup(config)
-          end
-        end
+        catppuccin-nvim
 
+        # Syntax and languages
+        (nvim-treesitter.withPlugins (plugins:
+          with plugins; [
+            lua
+            python
+            typescript
+            javascript
+            go
+            rust
+            c
+            cpp
+            java
+            json
+            yaml
+            toml
+            html
+            css
+            markdown
+            markdown_inline
+            vim
+            vimdoc
+            query
+          ]))
+      ];
 
-        -- LSP Keymaps
-        vim.api.nvim_create_autocmd('LspAttach', {
-          group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-          callback = function(ev)
-            local opts = { buffer = ev.buf }
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-            vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-            vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-            vim.keymap.set('n', '<leader>wl', function()
-              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, opts)
-            vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          end,
-        })
+      extraLuaConfig = ''
+                -- Basic vim options
+                vim.g.mapleader = " "
+                vim.g.maplocalleader = " "
 
-        -- Completion setup
-        local cmp = require('cmp')
-        local luasnip = require('luasnip')
+                -- Options
+                vim.opt.number = true
+                vim.opt.relativenumber = true
+                vim.opt.mouse = 'a'
+                vim.opt.showmode = false
+                vim.opt.clipboard = 'unnamedplus'
+                vim.opt.breakindent = true
+                vim.opt.undofile = true
+                vim.opt.ignorecase = true
+                vim.opt.smartcase = true
+                vim.opt.signcolumn = 'yes'
+                vim.opt.updatetime = 250
+                vim.opt.timeoutlen = 300
+                vim.opt.splitright = true
+                vim.opt.splitbelow = true
+                vim.opt.list = true
+                vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+                vim.opt.inccommand = 'split'
+                vim.opt.cursorline = true
+                  vim.opt.scrolloff = 10
+                vim.opt.hlsearch = false
 
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-          mapping = cmp.mapping.preset.insert({
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
-          }),
-          sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-            { name = 'buffer' },
-            { name = 'path' },
-          },
-        })
+        		-- Add these with your other vim.opt settings
+        		vim.opt.tabstop = 2       -- Number of spaces tabs count for
+        		vim.opt.softtabstop = 2   -- Number of spaces a tab counts for during editing
+        		vim.opt.shiftwidth = 2    -- Number of spaces for indentation
+        		vim.opt.expandtab = true  -- Use spaces instead of tabs
 
-        require('gitsigns').setup()
-	require("config")
+                        -- Basic Keymaps
+                        vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+                        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+                        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+                        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+                        -- Theme setup
+                        vim.cmd.colorscheme("catppuccin")
+
+                        -- LSP Keymaps
+                        vim.api.nvim_create_autocmd('LspAttach', {
+                          group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                          callback = function(ev)
+                            local opts = { buffer = ev.buf }
+                            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                            vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+                            vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                            vim.keymap.set('n', '<leader>wl', function()
+                              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                            end, opts)
+                            vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+                            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+                            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                          end,
+                        })
+
+                	-- Debug kubectl loading
+                	  local status_ok, kubectl = pcall(require, "kubectl")
+                	  if not status_ok then
+                	    print("Failed to load kubectl plugin")
+                	    return
+                	  end
+                	  
+                	  -- Initialize kubectl with debug
+                	  print("Setting up kubectl")
+                	  kubectl.setup()
+                	  
+                	  -- Add explicit keybinding with debug
+                	  vim.keymap.set("n", "<leader>k", function()
+                	    print("Kubectl toggle triggered")
+                	    kubectl.toggle()
+                	  end, { noremap = true, silent = false })
+
+                        require('gitsigns').setup()
+                	require("config")
       '';
     };
 
-
-
-xdg.configFile = {
-      "nvim/lua/config/init.lua".source = ./config/init.lua;
-      "nvim/lua/config/treesitter.lua".source = ./config/treesitter.lua;
-      "nvim/lua/config/neo-tree.lua".source = ./config/neo-tree.lua;
-      "nvim/lua/config/lualine.lua".source = ./config/lualine.lua;
-      "nvim/lua/config/telescope.lua".source = ./config/telescope.lua;
-      "nvim/lua/config/git.lua".source = ./config/git.lua;
-      "nvim/lua/config/kubectl.lua".source = ./config/kubectl.lua;
+    xdg.configFile = {
+      "nvim/lua/config" = {
+        source = ./config;
+        recursive =
+          true; # This will copy all files in the directory recursively
+      };
     };
   };
 }
