4d6675002bcd0599e033574c8fa9987cc0c4244a
Cameron <cbadwork@gmail.com>
2025-01-25 06:53:21 +0000

    fixed cider

--- modules/programs/media/cider.nix
+++ modules/programs/media/cider.nix
@@ -1,12 +1,39 @@
-{ pkgs, ... }:
+{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }
+, appimageTools ? pkgs.appimageTools, lib ? pkgs.lib, fetchurl ? pkgs.fetchurl
+}:
 
-let
-  # Path to the AppImage in the Nix store
-  ciderAppImage =
-    /nix/store/60xa8xjbdaf33ic04w5smhjjy0c5bgnm-cider-linux-x64.AppImage;
+appimageTools.wrapType2 rec {
+  pname = "cider";
+  version = "2.3.2";
 
-  # Create a wrapper for the AppImage using appimage-run
-  cider-2 = pkgs.writeShellScriptBin "cider-2" ''
-    ${pkgs.appimage-run}/bin/appimage-run ${ciderAppImage}
+  src = fetchurl {
+    url =
+      "file:///home/cameron/music/cider-linux-x64.AppImage"; # Update this path
+    sha256 =
+      "0xkpzmabma0x1kxkm762fqshsngqs9qxdh2mdfzcfnfwxpgd85na"; # Update this hash
+  };
+
+  extraInstallCommands = ''
+    # Move the extracted AppImage binary to the correct location
+    mv $out/bin/${pname}-${version} $out/bin/${pname} || true
+
+    # Install desktop file and icons (if they exist)
+    if [ -e ${pname}.desktop ]; then
+      install -m 444 -D ${pname}.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Exec=AppRun' 'Exec=${pname}'
+    fi
+
+    if [ -d usr/share/icons ]; then
+      cp -r usr/share/icons $out/share
+    fi
   '';
-in cider-2
+
+  meta = with lib; {
+    description =
+      "A new look into listening and enjoying Apple Music in style and performance.";
+    homepage = "https://cider.sh/";
+    maintainers = [ maintainers.nicolaivds ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
