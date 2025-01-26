ec7d6d82a6dac8f4fdf2dfef550fe6611ee051de
Cameron <cbadwork@gmail.com>
2025-01-27 05:47:54 +1000

    fixed bluetooth issue

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -1,10 +1,19 @@
-{ config, lib, pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ bluez ];
+{ config, pkgs, ... }:
 
+{
+  # Essential kernel module
+  boot.kernelModules = [ "btintel" ];
+
+  # Official firmware package
+  hardware.firmware = [ pkgs.linux-firmware ];
+
+  # Bluetooth configuration
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings.General.Experimental = true; # Enable modern features
   };
 
-  services.blueman.enable = true;
+  # Critical for Intel stability
+  boot.extraModprobeConfig = "options btusb enable_autosuspend=0";
 }
