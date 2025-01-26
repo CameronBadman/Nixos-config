bc78f02a612bd8fd0fa7baa19ef25b5a95af8c94
Cameron <cbadwork@gmail.com>
2025-01-26 05:17:54 +0000

    fixed docker

--- modules/programs/development/containers/default.nix
+++ modules/programs/development/containers/default.nix
@@ -1,11 +1,13 @@
 { config, pkgs, lib, ... }:
 
-let
-  utils = import ../../utils.nix { inherit lib; };
-in
-{
+let utils = import ../../utils.nix { inherit lib; };
+in {
   imports = utils.getImports ./.;
 
+  # Enable Docker
+  virtualisation.docker.enable = true;
+
+  # Add Docker and related tools to system packages
   environment.systemPackages = with pkgs; [
     docker
     docker-compose
@@ -13,5 +15,7 @@ in
     kubectl
     minikube
   ];
-}
 
+  # Add your user to the Docker group
+  users.users.cameron = { extraGroups = [ "docker" ]; };
+}
