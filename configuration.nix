c8e5944dc80b6b1c781a40c02e74378a48c11c4b
Cameron <cbadwork@gmail.com>
2025-01-26 05:17:21 +0000

    prettified config.nix

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,9 @@
-{ config, lib, pkgs, inputs, ... }:
-{
-  imports = [
-    ./hardware-configuration.nix
-    ./modules
-    ./hosts
-    ./users
-  ];
+{ config, lib, pkgs, inputs, ... }: {
+  imports = [ ./hardware-configuration.nix ./modules ./hosts ./users ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
-  nixpkgs.config = lib.mkDefault {
-    allowUnfree = true;
-  };
+
+  nixpkgs.config = lib.mkDefault { allowUnfree = true; };
 
   system.stateVersion = "24.11";
 }
