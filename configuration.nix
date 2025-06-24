e6757bf9648da33a88ebbf08b0962bd948cfb114
Cameron <cbadwork@gmail.com>
2025-06-25 03:23:13 +1000

    updated config file

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,52 @@
-{ config, lib, pkgs, inputs, ... }: {
-  imports = [ ./hardware-configuration.nix ./modules ./hosts ./users ];
+{ config, pkgs, ... }:
 
+{
+  # Boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  # Time zone and locale
+  time.timeZone = "Australia/Brisbane";
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  # Console keymap
+  console.keyMap = "us";
+
+  # Enable flakes and allow unfree
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.config = lib.mkDefault { allowUnfree = true; };
-
-  system.stateVersion = "24.11";
+  # Essential system packages only
+  environment.systemPackages = with pkgs; [
+  # Browsers
+  firefox
+  google-chrome
+  
+  # File manager
+  nautilus
+  
+  # Terminal
+  kitty
+  
+  # Desktop utilities
+  grim
+  slurp
+  dunst
+  libnotify
+  rofi-wayland
+  waybar
+  hyprpaper
+  networkmanagerapplet
+  brightnessctl
+  neofetch
+  htop
+  usbutils
+  pciutils
+  lshw
+];  # System version
+  system.stateVersion = "24.05";
 }
-
